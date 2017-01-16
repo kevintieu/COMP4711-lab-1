30675aba0af6f1040b3e8bc14b5d9015b287f580
Kevin <Kevin@192.168.1.87>
2017-01-15 17:02:15 -0800

    Added myself as a student

--- index.php
+++ index.php
@@ -16,25 +16,38 @@ and open the template in the editor.
         $students = array();
         
         $first = new Student();
-$first->surname = "Doe";
-$first->first_name = "John";
-$first->add_email('home','john@doe.com');
-$first->add_email('work','jdoe@mcdonalds.com');
-$first->add_grade(65);
-$first->add_grade(75);
-$first->add_grade(55);
-$students['j123'] = $first;
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
 
-$second = new Student();
-$second->surname = "Einstein";
-$second->first_name = "Albert";
-$second->add_email('home','albert@braniacs.com');
-$second->add_email('work1','a_einstein@bcit.ca');
-$second->add_email('work2','albert@physics.mit.edu');
-$second->add_grade(95);
-$second->add_grade(80);
-$second->add_grade(50);
-$students['a456'] = $second;
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+
+        $third = new Student();
+        $third->surname = "Tieu";
+        $third->first_name = "Kevin";
+        $third->add_email('home','kevin@tieu.com');
+        $third->add_email('work1','k_tieu@bcit.ca');
+        $third->add_email('work2','kevintieu@owsla.com');
+        $third->add_grade(90);
+        $third->add_grade(85);
+        $third->add_grade(80);
+        $students['k789'] = $third;
+
+        ksort($students); // one of the many sort functions
 
         foreach($students as $student)
             echo $student->toString();
